f585cde2b467ec142ecd9646fc3305dc915d0cf6
domino3333 <busster@naver.com>
2026-02-19 16:27:32 +0900

    초기 설정 후 서버 실행 - 문제 없이 잘 되는 것 확인

---
+++ src/main/java/com/zeus/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.zeus.controller;
+
+import java.util.Locale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class HomeController {
+
+	@GetMapping("/")
+	public String home(Locale locale, Model model) {
+		
+		log.info(locale + "의 방문을 환영합니다.");
+		model.addAttribute("serverTime", "2026-02-19");
+		return "home";
+	}
+}
